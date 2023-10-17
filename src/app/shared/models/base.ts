32755163124cb94098cbda6a815b7f2b3a5239d9
Gayatri Gaikwad <gayatrigaikwad5174@gmail.com>
2023-10-17 19:23:07 +0530

    shared module commit

---
+++ src/app/shared/models/base.ts
@@ -0,0 +1,14 @@
+export class Base {
+    createdBy: number;
+    createdOn: Date;
+    modifiedOn: Date;
+    modifiedBy: number;
+
+    constructor(){
+        this.createdBy = 0;
+        this.createdOn = new Date;
+        this.modifiedOn = new Date;
+        this.modifiedBy = 0 ;
+
+    }
+}
